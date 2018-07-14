ec955dd49b69c96bf1e22208bdd113eeb1099359
Alex Floor <Alex.Floor@topicus.nl>
2018-07-14 09:49:31 +0200

    initial commit including IDP

---
+++ IDP/Config.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser {
+                    SubjectId = "bedbd487-4df1-4514-ace2-367db9c08715",
+                    Username = "Alex",
+                    Password = "Ab12345!",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Alex"),
+                        new Claim("family_name", "Floor")
+                    }
+                },
+                new TestUser {
+                    SubjectId = "e5426a3e-3a84-4cb6-bed6-787df159b54a",
+                    Username = "Test",
+                    Password = "Ab12345!",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Test"),
+                        new Claim("family_name", "Tester")
+                    }
+                }
+            };
+        }
+
+        // identity-related resources (scopes)
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
---
+++ IDP/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IDP
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                    .AddTestUsers(Config.GetUsers())
+                    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                    .AddInMemoryClients(Config.GetClients())
+                    .AddDeveloperSigningCredential();   // TODO: replace for a real cerfiticate before moving to production
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
