4b23204683d784d3933f62f52a84a78244e47f69
Alex Floor <Alex.Floor@topicus.nl>
2018-07-16 08:58:24 +0200

    API protected with Authorisation + included client project (React + Redux)

--- IDP/Config.cs
+++ IDP/Config.cs
@@ -1,52 +1,91 @@
 ﻿using System.Collections.Generic;
 using System.Security.Claims;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 
 namespace IDP
 {
-    public static class Config
-    {
-        public static List<TestUser> GetUsers()
-        {
-            return new List<TestUser>
-            {
-                new TestUser {
-                    SubjectId = "bedbd487-4df1-4514-ace2-367db9c08715",
-                    Username = "Alex",
-                    Password = "Ab12345!",
-                    Claims = new List<Claim>
-                    {
-                        new Claim("given_name", "Alex"),
-                        new Claim("family_name", "Floor")
-                    }
-                },
-                new TestUser {
-                    SubjectId = "e5426a3e-3a84-4cb6-bed6-787df159b54a",
-                    Username = "Test",
-                    Password = "Ab12345!",
-                    Claims = new List<Claim>
-                    {
-                        new Claim("given_name", "Test"),
-                        new Claim("family_name", "Tester")
-                    }
-                }
-            };
-        }
+	public static class Config
+	{
+		public static List<TestUser> GetUsers()
+		{
+			return new List<TestUser>
+			{
+				new TestUser
+				{
+					SubjectId = "bedbd487-4df1-4514-ace2-367db9c08715",
+					Username = "Alex",
+					Password = "Ab12345!",
+					Claims = new List<Claim>
+					{
+						new Claim("given_name", "Alex"),
+						new Claim("family_name", "Floor")
+					}
+				},
+				new TestUser
+				{
+					SubjectId = "e5426a3e-3a84-4cb6-bed6-787df159b54a",
+					Username = "Test",
+					Password = "Ab12345!",
+					Claims = new List<Claim>
+					{
+						new Claim("given_name", "Test"),
+						new Claim("family_name", "Tester")
+					}
+				}
+			};
+		}
 
-        // identity-related resources (scopes)
-        public static IEnumerable<IdentityResource> GetIdentityResources()
-        {
-            return new List<IdentityResource>
-            {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile()
-            };
-        }
+		// identity-related resources (scopes)
+		public static IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new List<IdentityResource>
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile()
+			};
+		}
 
-        public static IEnumerable<Client> GetClients()
-        {
-            return new List<Client>();
-        }
-    }
+		public static IEnumerable<ApiResource> GetApiResources()
+		{
+			return new List<ApiResource>
+			{
+				new ApiResource("taxationexpertapi", "Taxation Expert API")
+			};
+		}
+
+		public static IEnumerable<Client> GetClients()
+		{
+			return new List<Client>
+			{
+				new Client
+				{
+					ClientName = "Taxation Expert",
+					ClientId = "TaxationExpertWebClient",
+					AllowedGrantTypes = GrantTypes.Hybrid,
+					RedirectUris = new List<string>
+					{
+						"https://localhost:8000/signin-oidc"
+					},
+					PostLogoutRedirectUris = new List<string>
+					{
+						"https://localhost:8000/signout-callback-oidc"
+					},
+					AllowedScopes =
+					{
+						IdentityServerConstants.StandardScopes.OpenId,
+						IdentityServerConstants.StandardScopes.Email,
+						IdentityServerConstants.StandardScopes.Profile,
+						"roles",
+						"taxationexpertapi"
+					},
+					ClientSecrets =
+					{
+						new Secret("secret".Sha256())
+					}
+				}
+			};
+		}
+	}
 }
--- IDP/Startup.cs
+++ IDP/Startup.cs
@@ -7,43 +7,44 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace IDP
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
 
-            services.AddIdentityServer()
-                    .AddTestUsers(Config.GetUsers())
-                    .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                    .AddInMemoryClients(Config.GetClients())
-                    .AddDeveloperSigningCredential();   // TODO: replace for a real cerfiticate before moving to production
-        }
+			services.AddIdentityServer()
+				.AddTestUsers(Config.GetUsers())
+				.AddInMemoryIdentityResources(Config.GetIdentityResources())
+				.AddInMemoryApiResources(Config.GetApiResources())
+				.AddInMemoryClients(Config.GetClients())
+				.AddDeveloperSigningCredential(); // TODO: replace for a real cerfiticate before moving to production
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				app.UseHsts();
+			}
 
-            app.UseIdentityServer();
-            app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
-        }
-    }
+			app.UseIdentityServer();
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
+		}
+	}
 }
